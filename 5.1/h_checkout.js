91fb49448ba53bfbd8e68f4e9c8c55c35e858770
zun95 <ilvinchan95@hotmail.com>
2018-10-02 18:19:09 +0900

    add delay wait()

--- 5.1/h_checkout.js
+++ 5.1/h_checkout.js
@@ -1,7 +1,6 @@
 function h_checkout(){
    if(document.querySelector("[class='button checkout']").href.includes("checkout")){
-      for(var i =0;i<50000000;i++){
-      }
+      
       document.querySelector("[class='button checkout']").click();
    };
 }
--- 5.1/index.js
+++ 5.1/index.js
@@ -5,28 +5,36 @@ document.addEventListener('DOMContentLoaded', function() {
   function reloadtab(i){
     chrome.tabs.update({url:i});
   }
+  function wait(x){
+    for(var i =0;i<x;i++){
+    }
+  }
   checkPageButton.addEventListener('click', function() {
     chrome.tabs.update({
          url: "https://www.supremenewyork.com/shop/all"
     });
     chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+       wait(80000000);
        if(changeInfo.status == "complete"){
          chrome.tabs.getSelected(null,function(tab) {
-           if(tab.title == "Supreme"){
+           if(tab.url=="https://www.supremenewyork.com/checkout"){
+             chrome.tabs.executeScript(null,{file:"fill.js"});
+           }else if(tab.title.includes("Supreme:")){
+             // chrome.tabs.query({active:true,windowType:"normal", currentWindow: true},function(d){console.debug(d);});
+             chrome.tabs.executeScript(null,{file:"putcart.js"});
+             wait(80000000);
+             chrome.tabs.executeScript(null,{file:"h_checkout.js"});
+             // chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+             //   reloadtab(request);
+             // });
+           }else if(tab.title == "Supreme"){
              //open search
              chrome.tabs.executeScript(null,{code:"var item = 'R3co9jxijii';"});
              chrome.tabs.executeScript(null,{file:"search.js"});
              chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
                reloadtab(request);
              });
-           }else if(tab.title.includes("Supreme:")){
-             // chrome.tabs.query({active:true,windowType:"normal", currentWindow: true},function(d){console.debug(d);});
-             chrome.tabs.executeScript(null,{file:"putcart.js"});
-             chrome.tabs.executeScript(null,{file:"h_checkout.js"});
 
-             // chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-             //   reloadtab(request);
-             // });
            };
          });
       };
